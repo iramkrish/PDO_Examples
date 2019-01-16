1e62373c7455d6ae41fe9a7151adc68a424278af
ramkrishnan <iramkrish95@gmail.com>
2019-01-16 16:16:48 +0530

    PDO examples -use comment to explore options

--- connection.php
+++ connection.php
@@ -34,46 +34,47 @@ $statement = $connection->query("SELECT * FROM daily_posts");
 
 // example FETCH_OBJ
 // default fetch mode set through setAttribute method in line 26
-// while($row = $statement->fetch(PDO::FETCH_OBJ)){
-//     echo $row->title;
-// }
+while($row = $statement->fetch(PDO::FETCH_OBJ)){
+    echo $row->title;
+}
 
 // example FETCH_ASSOC
 // default fetch mode set through setAttribute method in line 26 is overloaded
-// while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-//     echo $row['title'];
-// }
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    echo $row['title'];
+}
 
 // setting variables for testing purpose
 $author = 'cricketnext';
 
 // positional params
-// $sql_query = "SELECT * FROM daily_posts WHERE author=?";
-// $stmt = $connection->prepare($sql_query);
-// $stmt->execute([$author]);
-// $posts = $stmt->fetchAll();
-// var_dump($posts);
+$sql_query = "SELECT * FROM daily_posts WHERE author=?";
+$stmt = $connection->prepare($sql_query);
+$stmt->execute([$author]);
+$posts = $stmt->fetchAll();
+var_dump($posts);
 
 // named params
-// $sql_query = "SELECT * FROM daily_posts WHERE author=:author";
-// $stmt = $connection->prepare($sql_query);
-// // associative array
-// $stmt->execute(['author'=>$author]);
-// // default fetch mode set through setAttribute method in line 26
-// $posts = $stmt->fetchAll();
-// var_dump($posts);
+$sql_query = "SELECT * FROM daily_posts WHERE author=:author";
+$stmt = $connection->prepare($sql_query);
+// associative array
+$stmt->execute(['author'=>$author]);
+// default fetch mode set through setAttribute method in line 26
+$posts = $stmt->fetchAll();
+var_dump($posts);
 
 // INSERT DATA
-// $title = "email";
-// $body = "Ram Krishnan - iramkrish95@gmail.com";
-// $author = "ram";
-// $is_published = 1;
+$title = "email";
+$body = "Ram Krishnan - iramkrish95@gmail.com";
+$author = "ram";
+$is_published = 1;
 
-// $sql_insert = "INSERT INTO daily_posts(title,body,author,is_published) VALUES (:title,:body,:author,:is_published)";
-// $stmt_insert = $connection->prepare($sql_insert);
-// $stmt_insert->execute(["title"=>$title,"body"=>$body,"author"=>$author,"is_published"=>$is_published]);
-// echo "inserted";
+$sql_insert = "INSERT INTO daily_posts(title,body,author,is_published) VALUES (:title,:body,:author,:is_published)";
+$stmt_insert = $connection->prepare($sql_insert);
+$stmt_insert->execute(["title"=>$title,"body"=>$body,"author"=>$author,"is_published"=>$is_published]);
+echo "inserted";
 
+// UPDATE DATA
 $author = "ram krishnan";
 $id = 2;
 $sql_insert = "UPDATE daily_posts SET author=:author WHERE id=:id";
